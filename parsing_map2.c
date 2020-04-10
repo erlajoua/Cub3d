6848bd2452ffe296583550e428d6e218b93f8e1b
Erwan Lajouannique <er.lajouannique@gmail.com>
2020-04-11 00:46:32 +0200

    0 fonction de + de 25 lignes :pig:

--- parsing_map2.c
+++ parsing_map2.c
@@ -11,35 +11,19 @@ void line_check(t_cub cub, int j)
 
 		s = ft_strdup(" 1");
 		if (i == 0 && find_in(cub.parse.map[j][0], s) == 0)
-		{
 			ft_error("Mauvais debut de chaine\n");
-		}
 		if (cub.parse.map[j][i] == ' ')
-		{
 			if (!check_around(cub, j, i, " 1"))
-			{
 				ft_error("Map Error : Check_Space\n");
-			}
-		}
 		if (cub.parse.map[j][i] == '1')
-		{
 			if (!check_around(cub, j, i, " 012"))
-			{
 				ft_error("Map Error : Check_One\n");
-			}
-		}
 		if (cub.parse.map[j][i] == '0')
-		{
 			if (!check_around(cub, j, i, "012"))
-			{
 				ft_error("Map Error : Check_Zr\n");
-			}
-		}
 		if (cub.parse.map[j][i] == '2')
-		{
 			if (!check_around(cub, j, i, "012"))
 				ft_error("Map Error : Check_Two\n");
-		}
 		i++;
 	}
 }
@@ -82,26 +66,21 @@ int		check_around(t_cub cub, int j, int i, char *s)
 {
 	if (i == 0)
 	{
-		if (find_in(cub.parse.map[j][i + 1], s) && find_in(cub.parse.map[j - 1][i], s) && find_in(cub.parse.map[j + 1][i], s))
-		{
+		if (find_in(cub.parse.map[j][i + 1], s) && find_in(cub.parse.map[j - 1][i], s) 
+		&& find_in(cub.parse.map[j + 1][i], s))
 			return 1;
-		}
 	}
 	if (i == cub.parse.strlen - 1)
 	{
 		if (find_in(cub.parse.map[j][i - 1], s) && find_in(cub.parse.map[j - 1][i], s)
 		&& find_in(cub.parse.map[j + 1][i], s))
-		{
 			return 1;
-		}
 	}
 	else
 	{
-		if (find_in(cub.parse.map[j][i - 1], s)&& find_in(cub.parse.map[j][i + 1], s) 
+		if (find_in(cub.parse.map[j][i - 1], s) && find_in(cub.parse.map[j][i + 1], s) 
 		&& find_in(cub.parse.map[j - 1][i], s) && find_in(cub.parse.map[j + 1][i], s))
-		{
 			return 1;
-		}
 	}
 	return 0;
 }
