a0531fd0cad9827554f95b30a8041ce6e405598e
Erwan Lajouannique <er.lajouannique@gmail.com>
2020-04-11 10:21:36 +0200

    .

--- parsing_map2.c
+++ parsing_map2.c
@@ -7,8 +7,9 @@ void line_check(t_cub cub, int j)
 	i = 0;
 	while (cub.parse.map[j][i])
 	{
-		if (i == 0 && find_in(cub.parse.map[j][0], s) == 0)
-			ft_error("Mauvais debut de chaine\n");
+		char *s;
+
+		s = ft_strdup(" 1");
 		if (cub.parse.map[j][i] == ' ')
 			if (!check_around(cub, j, i, " 1"))
 				ft_error("Map Error : Check_Space\n");
@@ -57,21 +58,21 @@ int		check_around(t_cub cub, int j, int i, char *s)
 {
 	if (i == 0)
 	{
-		if (find_in(cub.parse.map[j][i + 1], s) && find_in(cub.parse.map[j - 1][i], s)
+		if (find_in(cub.parse.map[j][i + 1], s) && find_in(cub.parse.map[j - 1][i], s) 
 		&& find_in(cub.parse.map[j + 1][i], s))
-			return (1);
+			return 1;
 	}
 	if (i == cub.parse.strlen - 1)
 	{
 		if (find_in(cub.parse.map[j][i - 1], s) && find_in(cub.parse.map[j - 1][i], s)
 		&& find_in(cub.parse.map[j + 1][i], s))
-			return (1);
+			return 1;
 	}
 	else
 	{
-		if (find_in(cub.parse.map[j][i - 1], s) && find_in(cub.parse.map[j][i + 1], s)
+		if (find_in(cub.parse.map[j][i - 1], s) && find_in(cub.parse.map[j][i + 1], s) 
 		&& find_in(cub.parse.map[j - 1][i], s) && find_in(cub.parse.map[j + 1][i], s))
-			return (1);
+			return 1;
 	}
-	return (0);
+	return 0;
 }
