dc8969fd5f240811f4fcc9740466d63376585a0e
Thomas <thomassarafian@gmail.com>
2020-04-11 09:54:56 +0200

    norme

--- parsing_map2.c
+++ parsing_map2.c
@@ -7,9 +7,6 @@ void line_check(t_cub cub, int j)
 	i = 0;
 	while (cub.parse.map[j][i])
 	{
-		char *s;
-
-		s = ft_strdup(" 1");
 		if (i == 0 && find_in(cub.parse.map[j][0], s) == 0)
 			ft_error("Mauvais debut de chaine\n");
 		if (cub.parse.map[j][i] == ' ')
@@ -60,21 +57,21 @@ int		check_around(t_cub cub, int j, int i, char *s)
 {
 	if (i == 0)
 	{
-		if (find_in(cub.parse.map[j][i + 1], s) && find_in(cub.parse.map[j - 1][i], s) 
+		if (find_in(cub.parse.map[j][i + 1], s) && find_in(cub.parse.map[j - 1][i], s)
 		&& find_in(cub.parse.map[j + 1][i], s))
-			return 1;
+			return (1);
 	}
 	if (i == cub.parse.strlen - 1)
 	{
 		if (find_in(cub.parse.map[j][i - 1], s) && find_in(cub.parse.map[j - 1][i], s)
 		&& find_in(cub.parse.map[j + 1][i], s))
-			return 1;
+			return (1);
 	}
 	else
 	{
-		if (find_in(cub.parse.map[j][i - 1], s) && find_in(cub.parse.map[j][i + 1], s) 
+		if (find_in(cub.parse.map[j][i - 1], s) && find_in(cub.parse.map[j][i + 1], s)
 		&& find_in(cub.parse.map[j - 1][i], s) && find_in(cub.parse.map[j + 1][i], s))
-			return 1;
+			return (1);
 	}
-	return 0;
+	return (0);
 }
