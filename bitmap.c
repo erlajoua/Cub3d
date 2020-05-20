129b3fa69ea54e749ca31096d254c465231a72e4
ThomasSarafian <thomassarafian@gmail.com>
2020-05-20 16:16:00 +0200

    screenshot.bmp

--- bitmap.c
+++ bitmap.c
@@ -45,35 +45,53 @@ void	vertical_pix(t_mlx *mlx, int line, int *i, int j)
 		*i = *i + 1;
 	}
 }
-
-void	vertical(t_mlx *mlx)
+void vertical(int fd, t_mlx *mlx)
 {
-	{
-		int line;
-		int i;
-		int j;
+	int y;
+	int x;
+	int hex;
 
-		line = 0;
-		while (line < mlx->img.height)
+	y = mlx->img.height -1;
+	while (y >= 0)
+	{
+		x = 0;
+		while (x < mlx->img.width)
 		{
-			i = 0;
-			j = mlx->img.size_l;
-			while (i < j && i != j)
-			{
-				vertical_pix(mlx, line, &i, j);
-				j -= 4;
-			}
-			line++;
+			hex = mlx->img.data[y * mlx->img.width + x];
+			write(fd, &hex, sizeof(int));
+			x++;
 		}
+		y--;
 	}
 }
+// void	vertical(t_mlx *mlx)
+// {
+// 	{
+// 		int line;
+// 		int i;
+// 		int j;
+
+// 		line = 0;
+// 		while (line < mlx->img.height)
+// 		{
+// 			i = 0;
+// 			j = mlx->img.size_l;
+// 			while (i < j && i != j)
+// 			{
+// 				vertical_pix(mlx, line, &i, j);
+// 				j -= 4;
+// 			}
+// 			line++;
+// 		}
+// 	}
+// }
 
 void	header_bmp(t_mlx *mlx, int fd)
 {
 	int header_size;
 	int nb_plane;
 	int i;
-
+	
 	i = 0;
 	nb_plane = 1; //toujours 1
 	header_size = 40;
@@ -97,15 +115,18 @@ void	bitmap(t_mlx *mlx)
 	char *filename;
 
 	filename = ft_strdup("screen.bmp");
-	fd = open(filename, O_CREAT | O_RDWR);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0644);
 	file_size = 58 + (mlx->img.width * mlx->img.height) * 4;
 	begin_file = 58;
 	write(fd, "BM", 2);
 	write(fd, &file_size, 4);
 	write(fd, "\0\0\0\0", 4);
+	int offset = 58;
+	write(fd, &offset, 4);
+
 	header_bmp(mlx, fd);
-	vertical(mlx);
-	fill_data(mlx, fd);
+	vertical(fd, mlx);
+	// fill_data(mlx, fd);
 	close(fd);
 	free(filename);
 }
