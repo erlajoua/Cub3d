8677fbc183b1f14a8767a2f1c8ab337a238f5f74
ErwanLajouannique <er.lajouannique@gmail.com>
2020-06-04 00:06:17 +0200

    reduce window good

--- cub3d.c
+++ cub3d.c
@@ -6,7 +6,7 @@
 /*   By: tsarafia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/26 23:03:53 by tsarafia          #+#    #+#             */
-/*   Updated: 2020/05/26 23:04:03 by tsarafia         ###   ########.fr       */
+/*   Updated: 2020/06/03 21:24:31 by tsarafia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,21 @@ void	free_stuff(t_cub *cub)
 	free(cub->parse.sprite);
 }
 
+int	expose_hook(void *params)
+{
+	void	**recup;
+
+	t_info		*infos;
+	t_mlx		*mlx;
+	t_cub		*cub;
+	recup = (void **)params;
+	infos = recup[0];
+	mlx = recup[1];
+	cub = recup[2];	
+	all(((t_info *)recup[0]), ((t_mlx *)recup[1]), ((t_cub *)recup[2]));
+	return 0;
+}
+
 int		main(int ac, char **av)
 {
 	void	*params[3];
@@ -32,7 +47,6 @@ int		main(int ac, char **av)
 		check_ext(av[1], ".cub");
 	else
 		ft_error("No map");
-	// form_parse(av[1]);
 	ft_start(&cub);
 	get_lines(&cub, &infos, av[1]);
 	fill_sp(&cub);
@@ -44,9 +58,13 @@ int		main(int ac, char **av)
 	params[2] = (void *)&cub;
 	raycasting(&mlx, &infos, &cub);
 	free_stuff(&cub);
-	// form_parse2(&cub, av[1]);
+	
+	
 	mlx_hook(mlx.win, 2, (1L << 0), keypressed, (void *)params);
 	mlx_hook(mlx.win, 17, (1L << 17), cross_destroy, (void *)(&params));
+	
+	mlx_expose_hook(mlx.win, expose_hook, (void *)params);
 	mlx_loop(mlx.mlx_ptr);
+	printf("WTF!!!\n");
 	return (0);
 }
